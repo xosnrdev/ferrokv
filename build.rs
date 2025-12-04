bedd4490e7742555efa67e8b91e2b43cbc13912c
Success Kingsley <successxodev@gmail.com>
2025-12-04 07:48:25 -0500

    remove: tokio handles the check

--- build.rs
+++ build.rs
@@ -3,6 +3,4 @@ fn main() {
     unsafe {
         std::env::set_var("RUSTFLAGS", "--cfg tokio_unstable");
     };
-    #[cfg(all(feature = "io-uring", not(target_os = "linux")))]
-    panic!("The `io-uring` feature is only supported on Linux.");
 }
